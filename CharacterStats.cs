40b95f3ed42989f8deacc08fd72d236364353b2d
Dracon <petermarkcarey@gmail.com>
2021-05-22 21:44:24 +0800

    init

---
+++ CharacterStats.cs
@@ -0,0 +1,103 @@
+﻿namespace DI_Statistics
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class CharacterStats : MonoBehaviour
+    {
+        public GameObject floatingTextPrefab;
+        public Transform floatingTextSpawnPoint;
+
+        public string characterIdentifier;
+
+        public float maxHealth;
+        public float currentHealth { get; private set; }
+
+        public float maxMana;
+        public float currentMana { get; private set; }
+
+        public float maxStamina;
+        public float currentStamina { get; private set; }
+
+        public float STR, DEX, WIS, INT, CON, LUCK, WILL;
+
+        public float damage;
+        public float armour;
+
+        public float awareness, stealthDetection, viewDistance;
+
+        [HideInInspector]
+        public Animator anim;
+        private void Awake()
+        {
+            maxHealth = Calculate_MaxHealth();
+            currentHealth = maxHealth;
+        }
+
+        private void Start()
+        {
+            VStart();
+        }
+
+        public virtual void VStart()
+        {
+
+        }
+
+        private void Update()
+        {
+            VUpdate();
+        }
+
+        public virtual void VUpdate()
+        {
+
+        }
+
+        public virtual void TakeDamage(float damage)
+        {
+            damage -= armour;
+            damage = Mathf.Clamp(damage, 0, int.MaxValue);
+            currentHealth -= damage;
+
+            Instantiate(floatingTextPrefab, transform.position + Vector3.up * 0.5f, Quaternion.identity).GetComponent<FloatingText>().Setup(damage.ToString(), floatingTextSpawnPoint.position);
+            Debug.Log(transform.name + " takes " + damage + " damage");
+
+            if (currentHealth <= 0)
+            {
+                Die();
+            }
+            else
+            {
+                if (anim != null)
+                {
+                    anim.SetTrigger("Damaged");
+                }
+                else
+                {
+                    print("No damage animator on " + gameObject.name);
+                }
+            }
+        }
+
+        public virtual void Die()
+        {
+            Debug.Log(transform.name + " died");
+            if (anim != null)
+            {
+                anim.SetTrigger("Death");
+            }
+            if (characterIdentifier.ToLower() != "player")
+            {
+                QuestController.Instance.QuestAction(Quest.Objective.Type.Kill, characterIdentifier, 1);
+            }
+        }
+
+        float Calculate_MaxHealth()
+        {
+            return (3 * CON) + (1.5f * STR);
+        }
+    }
+}
---
+++ CharacterStatsEditor.cs
@@ -0,0 +1,58 @@
+﻿namespace DI_Statistics
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEditor;
+    [CustomEditor(typeof(CharacterStats))]
+    [CanEditMultipleObjects]
+    public class CharacterStatsEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            //base.OnInspectorGUI();
+            CharacterStats myTarget = (CharacterStats)target;
+
+            myTarget.floatingTextPrefab = (GameObject)EditorGUILayout.ObjectField("Floating Text Prefab:", myTarget.floatingTextPrefab, typeof(GameObject), false);
+            myTarget.floatingTextSpawnPoint = (Transform)EditorGUILayout.ObjectField("Floating Text Spawn:", myTarget.floatingTextSpawnPoint, typeof(Transform), true);
+
+            myTarget.characterIdentifier = EditorGUILayout.TextField("ID: ", myTarget.characterIdentifier);
+
+            EditorGUILayout.Space();
+
+            DrawCharacterStats(myTarget, serializedObject);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        public static void DrawCharacterStats(CharacterStats myTarget, SerializedObject serializedObject)
+        {
+            myTarget.damage = EditorGUILayout.FloatField("Damage: ", myTarget.damage);
+            myTarget.armour = EditorGUILayout.FloatField("Armour: ", myTarget.armour);
+
+            Vector3 resourceStats = EditorGUILayout.Vector3Field("Resources Max (HMS): ", new Vector3(myTarget.maxHealth, myTarget.maxMana, myTarget.maxStamina));
+            myTarget.maxHealth = resourceStats.x;
+            myTarget.maxMana = resourceStats.y;
+            myTarget.maxStamina = resourceStats.z;
+
+            Vector3 sdc = EditorGUILayout.Vector3Field("STR, DEX, CON: ", new Vector3(myTarget.STR, myTarget.DEX, myTarget.CON));
+            myTarget.STR = sdc.x;
+            myTarget.DEX = sdc.y;
+            myTarget.CON = sdc.z;
+
+            Vector3 iww = EditorGUILayout.Vector4Field("INT, WIS, WILL, LUCK: ", new Vector4(myTarget.INT, myTarget.WIS, myTarget.WILL, myTarget.LUCK));
+            myTarget.INT = iww.x;
+            myTarget.WIS = iww.y;
+            myTarget.WILL = iww.z;
+
+            SerializedProperty awarenessProp = serializedObject.FindProperty("awareness");
+            EditorGUILayout.PropertyField(awarenessProp);
+
+            SerializedProperty stealthDetProp = serializedObject.FindProperty("stealthDetection");
+            EditorGUILayout.PropertyField(stealthDetProp);
+
+            SerializedProperty viewDistProp = serializedObject.FindProperty("viewDistance");
+            EditorGUILayout.PropertyField(viewDistProp);
+        }
+    }
+}
